5030a1a412e7cb10ef54423cae5571014e7a8427
Itiene Guilherme <64323258+Itiene@users.noreply.github.com>
2026-07-14 20:57:28 +0100

    Atualizado

--- costs/src/components/layout/LinkButton.js
+++ costs/src/components/layout/LinkButton.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import styles from './LinkButton.module.css'
 
-function LinkButton(to,text){
+function LinkButton({to,text}){
     return(
         <Link className={styles.btn} to={to}>
+            {text}
         </Link>
     )
 }
